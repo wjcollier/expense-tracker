e5600c032958758bf6c2f2b8de430c9f3b09811f
wjcollier <williamjcollier4@gmail.com>
2022-05-23 10:49:57 +0800

    1st commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
+
+const ExpenseItem =(data) => {
+
+  return (
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={data.date} />
+        <div className='expense-item__description'>
+          <h2>{data.title}</h2>
+          <div className='expense-item__price'>${data.amount}</div>
+        </div>
+      </Card>
+    </li>
+  )
+}
+
+export default ExpenseItem;
